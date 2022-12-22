7af1996299d5b68efe4f025f8a731bd4849ed9f4
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-22 10:06:53 +0100

    exo 2  Boucles FOR (let)

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
 // en js crée une variable de type array et insère dedans des noms de la classe et ensuite à l'aide d'une boucle for, dit 'bonjour'+ noms à chaque personnes du tableau
 
-let array = ['Quentin','Bachir','Sebastien','Brandon','Charlotte','Denis','Yassine']
+// let array = ['Quentin','Bachir','Sebastien','Brandon','Charlotte','Denis','Yassine']
 
-for (let index = 0; index < array.length; index++) {
-    console.log("Bonjour " + '' + array[index]);
+// for (let index = 0; index < array.length; index++) {
+//     console.log("Bonjour " + '' + array[index]);
+    
+// }
+
+// en js dans un prompt insere un nombre et à l'aide d'une boucle for, affiche les nombres de 0 jusqu'au nombre que vous avez choisi
+
+let nbr = prompt("Choisis un nombre jusqu'où est ce que le compteur s'arrêtera");
+nbr = parseInt(nbr);
+for (let index = 0; index <= nbr; index++) {
+    console.log(index);
     
 }
