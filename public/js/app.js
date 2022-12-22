77e46ec0c2259e53e6d7f6b7def8a6a8ced109d0
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-22 10:20:05 +0100

    exo 3  Boucles FOREACH

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,18 @@
 
 // en js dans un prompt insere un nombre et à l'aide d'une boucle for, affiche les nombres de 0 jusqu'au nombre que vous avez choisi
 
-let nbr = prompt("Choisis un nombre jusqu'où est ce que le compteur s'arrêtera");
-nbr = parseInt(nbr);
-for (let index = 0; index <= nbr; index++) {
-    console.log(index);
+// let nbr = prompt("Choisis un nombre jusqu'où est ce que le compteur s'arrêtera");
+// nbr = parseInt(nbr);
+// for (let index = 0; index <= nbr; index++) {
+//     console.log(index);
     
-}
+// }
+
+// en js crée une variable de type array et insère des personnes de la classe ensuite à l'aide d'une boucle foreach, dit 'bonjour' + noms
+
+let array = ['Quentin','Bachir','Sebastien','Brandon','Charlotte','Denis','Yassine'];
+
+
+array.forEach(function(array){
+    console.log('bonjour ' + array);
+});
